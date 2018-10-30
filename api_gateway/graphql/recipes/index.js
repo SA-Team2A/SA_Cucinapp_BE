e373bd6ba53981a86028754572d568b3b22b81ab
Raúl Coral <rfcoralj@unal.edu.co>
2018-10-30 01:21:54 -0500

    Recipes API gateway

---
+++ api_gateway/graphql/recipes/index.js
@@ -0,0 +1,33 @@
+const requireSchema = require('require-graphql-file')
+const schema = requireSchema('./recipes_schema')
+const { recipes_url } = require('../../request/urls')
+const { GET, POST, PUT, DELETE } = require('../../request')
+
+const resolvers = {
+  getRecipes: async () => {
+    let res = await GET(recipes_url, '/recipes')
+    return res
+  },
+  getRecipeById: async ({ _id }) => {
+    let res = await GET(recipes_url, `/recipes/${_id}`)
+    return res
+  },
+  searchRecipes: async ({ search }) => {
+    let res = await GET(recipes_url, '/recipes', search)
+    return res
+  },
+  createRecipe: async ({ recipe }) => {
+    let res = await POST(recipes_url, '/recipes', recipe)
+    return res
+  },
+  updateRecipe: async ({ _id, recipe }) => {
+    let res = await PUT(recipes_url, `/recipes/${_id}`, recipe)
+    return res
+  },
+  deleteRecipe: async ({ _id }) => {
+    let res = await DELETE(recipes_url, `/recipes/${_id}`)
+    return res
+  }
+}
+
+module.exports = { schema, resolvers }
